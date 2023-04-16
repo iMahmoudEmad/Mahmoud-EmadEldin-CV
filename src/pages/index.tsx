1eeec4adb4042f942def6068d43dec4339e27708
Mahmoud Emad Eldin <memad@kashier.io>
2023-04-17 01:51:46 +0200

    Make the personal image lazy loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             alt="Picture of the author"
             width={500}
             height={500}
+            loading="lazy"
           />
 
           <div className={styles.personInfo}>
